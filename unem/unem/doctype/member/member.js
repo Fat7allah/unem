21c62186804c34ee895685932e84f190c3d77614
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-11 21:31:16 +0100

    Update member.js

--- unem/unem/doctype/member/member.js
+++ unem/unem/doctype/member/member.js
@@ -11,6 +11,11 @@ frappe.ui.form.on('Member', {
             frm.doc.create_membership_card();
             frm.save();
         }, __('إجراءات'));
+        
+        // Clear province when form loads if no region is selected
+        if (!frm.doc.region) {
+            frm.set_value('province', '');
+        }
     },
     
     validate: function(frm) {
@@ -31,6 +36,33 @@ frappe.ui.form.on('Member', {
             frm.set_df_property('teaching_specialty', 'hidden', 1);
             frm.set_df_property('teaching_specialty', 'reqd', 0);
             frm.set_value('teaching_specialty', '');
+        }
+    },
+    
+    region: function(frm) {
+        // Clear province when region changes
+        frm.set_value('province', '');
+
+        // Set up filters for province field
+        frm.set_query('province', function() {
+            return {
+                filters: {
+                    'region': frm.doc.region
+                }
+            };
+        });
+    },
+
+    province: function(frm) {
+        // Validate that selected province belongs to selected region
+        if (frm.doc.province && frm.doc.region) {
+            frappe.db.get_value('Province', frm.doc.province, 'region')
+                .then(r => {
+                    if (r.message && r.message.region !== frm.doc.region) {
+                        frm.set_value('province', '');
+                        frappe.throw(__(`الإقليم المحدد لا ينتمي إلى ${frm.doc.region}`));
+                    }
+                });
         }
     }
 });
